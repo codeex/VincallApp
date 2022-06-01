0c06d6e3ead72331a25a6cef4500ed49842949df
mark <mark.hong@comm100.com>
2022-06-01 16:16:40 +0800

    add vincallCallback

--- src/pages/integration/SettingsPage.tsx
+++ src/pages/integration/SettingsPage.tsx
@@ -1,24 +1,75 @@
-import React, { MouseEvent } from 'react';
-import { CFormContent } from '../../components/integration/form/CFormContent';
-import { CIntegrationForm } from '../../components/integration/form/CIntegrationForm';
-import { CPage } from '../../components/CPage';
-import { CUnConnected } from '../../components/CUnConnected';
+import React, { MouseEvent, useEffect, useRef, useState } from "react";
+import { CFormContent } from "../../components/integration/form/CFormContent";
+import { CIntegrationForm } from "../../components/integration/form/CIntegrationForm";
+import { CPage } from "../../components/CPage";
+import { CUnConnected } from "../../components/CUnConnected";
+import { delayOpenWindow } from "@comm100/framework/Helpers";
+import { CCircularProgressStyled } from "@comm100/styledComponents/Button/CCircularProgressStyled";
+import { CVincallConnectContainerStyled } from "../../styledComponents/CVincallConnectContainerStyled";
 
 export const SettingsPage = () => {
+  const [isIntegrating, setIsIntegrating] = useState(false);
+  const handleRef = useRef(null as any);
   const clickGoBackHandle = (event: MouseEvent) => {
-    console.log('clickGoBackHandle');
+    console.log("clickGoBackHandle");
   };
   const clickConnectHandle = (event: MouseEvent) => {
-    console.log('clickConnectHandle');
+    handleRef.current = delayOpenWindow(
+      async () => {
+        return [
+          "https://oauth.vincall.net/connect/authorize",
+          "?client_id=vincall",
+          "&response_type=code",
+          "&scope=vincall",
+          `&redirect_uri=${encodeURIComponent(
+            "https://apivincall.comm100dev.io/open/login/callback?redirect_uri=https://voipdash.comm100dev.io/vincallback.html"
+          )}`
+        ].join("");
+      },
+      "connect",
+      {
+        width: 600,
+        height: 500,
+        top: 100,
+        left: 100
+      }
+    );
   };
+  const handleMessage = async (event: any) => {
+    const { code } = event.data;
+    const pathname = event.source.location.pathname;
+
+    if (pathname.toLowerCase().indexOf("vincallCallback.html") >= 0 && code) {
+      setIsIntegrating(true);
+
+      handleRef.current.then(h => h.close());
+
+      //TODO: get data
+
+      setIsIntegrating(false);
+    }
+  };
+  const handleLoad = () => {
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  };
+  useEffect(() => {
+    return handleLoad();
+  }, []);
   return (
     <CPage
-      id='settingsPage'
-      title='VinCall Integration'
-      description='Description'
+      id="settingsPage"
+      title="VinCall Integration"
+      description="Description"
       onClickGoBack={clickGoBackHandle}
     >
-      <CUnConnected onClick={clickConnectHandle} />
+      {isIntegrating ? (
+        <CVincallConnectContainerStyled>
+          <CCircularProgressStyled size={50} />
+        </CVincallConnectContainerStyled>
+      ) : (
+        <CUnConnected onClick={clickConnectHandle} />
+      )}
       <CIntegrationForm>
         <CFormContent />
       </CIntegrationForm>
---
+++ src/styledComponents/CVincallConnectContainerStyled.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { makeStyles, createStyles, Theme } from "@comm100/styledComponents";
+import { StyledComponentProps } from "@comm100/styledComponents/types";
+
+const useStyle = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      height: "400px",
+      position: "relative",
+      "& button": {
+        marginTop: theme.spacing(4),
+      },
+      "& img": {
+        marginBottom: theme.spacing(2),
+      },
+      "& p": {
+        position: "absolute",
+        bottom: 0,
+        margin: 0,
+        color: theme.palette.text.secondary,
+        fontSize: 12,
+      },
+    },
+  }),
+);
+
+export const CVincallConnectContainerStyled = (
+  props: StyledComponentProps<
+    React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLDivElement>,
+      HTMLDivElement
+    >
+  >,
+) => {
+  const classes = useStyle();
+  return <div className={classes.root} {...props} />;
+};
+CVincallConnectContainerStyled.displayName =
+  "CVincallConnectContainerStyled";
